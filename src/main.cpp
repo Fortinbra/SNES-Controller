b6983cf9512bff3233af80b9038d60210cac745e
Fortinbra <fortinbra@becauseimclever.com>
2024-10-08 21:29:26 -0500

    No more squigglies!

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,55 @@
 
 #define START 6
 
+const uint16_t SNES_BUTTON_A = 0x01;
+const uint16_t SNES_BUTTON_B = 0x02;
+const uint16_t SNES_BUTTON_X = 0x04;
+const uint16_t SNES_BUTTON_Y = 0x08;
+const uint16_t SNES_BUTTON_L = 0x10;
+const uint16_t SNES_BUTTON_R = 0x20;
+const uint16_t SNES_BUTTON_SELECT = 0x40;
+const uint16_t SNES_BUTTON_START = 0x80;
+const uint16_t SNES_BUTTON_UP = 0x100;
+const uint16_t SNES_BUTTON_DOWN = 0x200;
+const uint16_t SNES_BUTTON_LEFT = 0x400;
+const uint16_t SNES_BUTTON_RIGHT = 0x800;
+const uint16_t buttonOrder[16] = {
+	SNES_BUTTON_B,
+	SNES_BUTTON_Y,
+	SNES_BUTTON_SELECT,
+	SNES_BUTTON_START,
+	SNES_BUTTON_UP,
+	SNES_BUTTON_DOWN,
+	SNES_BUTTON_LEFT,
+	SNES_BUTTON_RIGHT,
+	SNES_BUTTON_A,
+	SNES_BUTTON_X,
+	SNES_BUTTON_L,
+	SNES_BUTTON_R,
+	0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF // Unknown buttons always report high
+};
+
+uint16_t snesState;
+
 void gpioIRQHandler(uint gpio, uint32_t events)
 {
+	gpio_put(DATA, ((snesState & buttonOrder[0]) != 0));
+	for (int i = 1; i < 16; i++)
+	{
+		while (gpio_get(CLK) == 1)
+		{
+			tight_loop_contents();
+		}
+		if (i < 12)
+		{
+			gpio_put(DATA, ((snesState & buttonOrder[i]) != 0));
+		}
+		else
+		{
+			gpio_put(DATA, 1);
+		}
+	}
+	gpio_put(DATA, 0);
 }
 
 int main()
@@ -21,6 +68,7 @@ int main()
 	uart_init(UART_ID, BAUD_RATE);
 	gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
 	gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+	snesState = 0x00;
 	gpio_init(DATA);
 	gpio_init(CLK);
 
